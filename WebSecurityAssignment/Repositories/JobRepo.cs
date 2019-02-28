1e1d9312b4f98b17041efc0db2947008f3c67c74
AJ Adversalo <ajadvers@gmail.com>
2019-02-28 10:25:38 -0800

    added jobsrepo

---
+++ WebSecurityAssignment/Repositories/JobRepo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebSecurityAssignment.Data;
+
+namespace WebSecurityAssignment.Repositories
+{
+    public class JobRepo
+    {
+        ApplicationDbContext _context;
+
+        public JobRepo(ApplicationDbContext context)
+        {
+            this._context = context;
+        }
+
+        public List<RoleVM> GetAllRoles()
+        {
+            var roles = _context.Roles;
+            List<RoleVM> roleList = new List<RoleVM>();
+
+            foreach (var item in roles)
+            {
+                roleList.Add(new RoleVM() { RoleName = item.Name, Id = item.Id });
+            }
+            return roleList;
+        }
+    }
+}
