bc6e2c0e2503766f4d52ca33c56ba2177ee832a8
Vilux <mschneider20@my.bcit.ca>
2019-02-28 11:59:04 -0800

    Added Ratings repo and controller

--- WebSecurityAssignment/Controllers/RatingsController.cs
+++ WebSecurityAssignment/Controllers/RatingsController.cs
@@ -2,14 +2,67 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using WebSecurityAssignment.Data;
+using WebSecurityAssignment.Repositories;
 
 namespace WebSecurityAssignment.Controllers
 {
+    [Authorize(Roles = "Admin")]
     public class RatingsController : Controller
     {
-        public IActionResult Index()
+        ApplicationDbContext _context;
+
+        public RatingsController(ApplicationDbContext context)
         {
+            _context = context;
+        }
+
+        // GET: Role
+        public ActionResult Index()
+        {
+            RatingsRepo ratingsRepo = new RatingsRepo(_context);
+            return View(ratingsRepo.GetAllRatings());
+        }
+
+        [HttpGet]
+        public ActionResult Delete()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Delete(Ratings rating)
+        {
+            var ratings = _context.Ratings;
+            if (ModelState.IsValid)
+            {
+                RatingsRepo ratingsRepo = new RatingsRepo(_context);
+                var success = ratingsRepo.RemoveRating(rating.employeeID, rating.jobID);
+                if (success)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            ViewBag.Error = "An error occurred while deleting this role. Please try again.";
+            return View();
+        }
+
+        [HttpPut]
+        public ActionResult Update(Ratings rating)
+        {
+            var ratings = _context.Ratings;
+            if (ModelState.IsValid)
+            {
+                RatingsRepo ratingsRepo = new RatingsRepo(_context);
+                var success = ratingsRepo.UpdateRating(rating.jobID, rating.employeeID, rating.review, rating.score);
+                if (success)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            ViewBag.Error = "An error occurred while deleting this role. Please try again.";
             return View();
         }
     }
