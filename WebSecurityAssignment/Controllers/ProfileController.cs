66b7d8c3abb1cb7c4bdcd92045816d692bd1b747
stephen syrnyk <stephen.syrnyk@gmail.com>
2019-03-05 15:54:34 -0800

    Forced conflict

--- WebSecurityAssignment/Controllers/ProfileController.cs
+++ WebSecurityAssignment/Controllers/ProfileController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using WebSecurityAssignment.Data;
 using WebSecurityAssignment.Repositories;
 
@@ -22,7 +24,8 @@ namespace WebSecurityAssignment.Controllers
 
         public IActionResult Index(string id)
         {
-            var listings = _context.Jobs.Where(j => j.employerID == id);
+            var listings = _context.Jobs.Where(j => j.employerID == id);           
+
             return View(listings.ToList());
         }
     }
