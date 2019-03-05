52eca620e18b634a75ec7421161d775737827b7c
AJ Adversalo <ajadvers@gmail.com>
2019-03-05 11:51:43 -0800

    added user name fields to uservm

--- WebSecurityAssignment/ViewModels/UserVM.cs
+++ WebSecurityAssignment/ViewModels/UserVM.cs
@@ -8,5 +8,7 @@ namespace WebSecurityAssignment.ViewModels
 	public class UserVM
 	{
 		public string Email { get; set; }
-	}
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
 }
