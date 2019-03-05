0802a106276931ffb6f67d8f432ea37e85c70ef4
AJ Adversalo <ajadvers@gmail.com>
2019-03-05 13:45:32 -0800

    added applicationVM class

--- WebSecurityAssignment/ViewModels/ApplicationVM.cs
+++ WebSecurityAssignment/ViewModels/ApplicationVM.cs
@@ -7,5 +7,12 @@ namespace WebSecurityAssignment.ViewModels
 {
     public class ApplicationVM
     {
+        public string EmployerFN { get; set; }
+        public string EmployerLN { get; set; }
+        public string EmployeeFN { get; set; }
+        public string EmployeeLN { get; set; }
+        public string JobTitle { get; set; }
+        public int    JobID { get; set; }
+        public string comments { get; set; }
     }
 }
