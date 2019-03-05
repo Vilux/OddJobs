5235a124d7e43ac4fceb35e1f8e2662062734e19
Vilux <mschneider20@my.bcit.ca>
2019-03-05 13:38:42 -0800

    tryping to pull

---
+++ WebSecurityAssignment/Migrations/20190305185619_addedAddress.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebSecurityAssignment.Migrations
+{
+    public partial class addedAddress : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "review",
+                table: "Ratings",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "AspNetUsers",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "review",
+                table: "Ratings");
+
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "AspNetUsers");
+        }
+    }
+}
---
+++ WebSecurityAssignment/Repositories/ApplicationRepo.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WebSecurityAssignment.Data;
+
+namespace WebSecurityAssignment.Repositories
+{
+    class ApplicationRepo
+    {
+        ApplicationDbContext _context;
+
+        public ApplicationRepo (ApplicationDbContext context)
+        {
+            this._context = context;
+        }
+
+        public List<Application> GetAllApplications()
+        {
+            var applications = _context.Applications;
+            List<Application> applicationList = new List<Application>();
+
+            foreach (var item in applications)
+            {
+                applicationList.Add(new Application()
+                {
+                    ApplicantID = item.ApplicantID,
+                    JobID = item.JobID,
+                    Comment = item.Comment
+                });
+            }
+            return applicationList;
+        }
+
+        public Application GetApplication(string ApplicantID, int JobID)
+        {
+            var application = _context.Applications.Where(a => a.ApplicantID == ApplicantID && a.JobID == JobID).FirstOrDefault();
+            if (application != null)
+            {
+                return new Application()
+                {
+                    ApplicantID = application.ApplicantID,
+                    JobID = application.JobID,
+                    Comment = application.Comment
+                };
+            }
+            return null;
+        }
+
+        public bool RemoveApplication(string ApplicantID, int JobID)
+        {
+            var application = _context.Applications.Where(a => a.ApplicantID == ApplicantID && a.JobID == JobID).FirstOrDefault();
+
+            _context.Applications.Remove(application);
+            _context.SaveChanges();
+            return true;
+        }
+
+        public bool UpdateApplication(string ApplicantID, int JobID, string Comment)
+        {
+            var application = _context.Applications.Where(a => a.ApplicantID == ApplicantID && a.JobID == JobID).FirstOrDefault();
+            // Remember you can't update the primary key without 
+            // causing trouble.  Just update the review and score
+            // for now.
+            application.Comment = Comment;
+
+            _context.SaveChanges();
+            return true;
+        }
+
+        public bool CreateApplication(string ApplicantID, int JobID, string Comment)
+        {
+            var application = GetApplication(ApplicantID, JobID);
+            if (application != null)
+            {
+                return false;
+            }
+            _context.Applications.Add(new Application
+            {
+                ApplicantID = ApplicantID,
+                JobID = JobID,
+                Comment = Comment
+            });
+            _context.SaveChanges();
+            return true;
+        }
+    }
+}
