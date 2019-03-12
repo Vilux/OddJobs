41ffa7c03ad0360d6e869e6e067d143f738b49d9
jlperrella <jlperrella@gmail.com>
2019-03-12 14:39:53 -0700

    edits to API

--- WebSecurityAssignment/APIs/JobFeedAPI/JobFeedAPIController.cs
+++ WebSecurityAssignment/APIs/JobFeedAPI/JobFeedAPIController.cs
@@ -4,66 +4,35 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using WebSecurityAssignment.Data;
-
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+using WebSecurityAssignment.Repositories;
+using WebSecurityAssignment.ViewModels;
 
 namespace WebSecurityAssignment.APIs.JobFeedAPI
 {
     [Route("api/[controller]")]
     public class JobFeedAPIController : Controller
     {
- 
         private readonly ApplicationDbContext Dbcontext;
-
         public JobFeedAPIController(ApplicationDbContext context) {
         Dbcontext = context;
         }
 
-
-        // GET: api/<controller>
         [HttpGet]
-        [Route("Get")]
-        public IEnumerable<Job> Get()
+        [Route("GetAll")]
+        public IEnumerable<JobVM> Get()
         {
-            //return new string[] { "value1", "value2" };
-            return Dbcontext.Jobs.ToList();
-        }
-
-        
-        [HttpGet("{id}", Name = "GetJobs")]
-        public IActionResult GetById(int id) {
-            var item = Dbcontext.Jobs.FirstOrDefault(t => t.jobID == id);
-            if (item == null) {
-                return NotFound();
-            }
-            return new ObjectResult(item);
+            JobRepo jobRepo = new JobRepo(Dbcontext);
+            var jobs = jobRepo.GetAllJobs();
+            return jobs.ToList();
         }
 
         /*
-        // GET api/<controller>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-        */
-        // POST api/<controller>
-        [HttpPost]
-        public void Post([FromBody]string value)
-        {
-
-        }
-
-        // PUT api/<controller>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/<controller>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
+      [HttpGet]
+      [Route("Get")]
+      public IEnumerable<Job> Get()
+      {
+          return Dbcontext.Jobs.ToList();
+      }
+      */
     }
 }
