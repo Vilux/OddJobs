2993680b6a931ea8f7da65ac5065d80126204edf
AJ Adversalo <ajadvers@gmail.com>
2019-03-12 09:59:48 -0700

    add logged in authorization to applications controller

--- WebSecurityAssignment/Controllers/ApplicationsController.cs
+++ WebSecurityAssignment/Controllers/ApplicationsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -10,6 +11,7 @@ using WebSecurityAssignment.Repositories;
 
 namespace WebSecurityAssignment.Controllers
 {
+    [Authorize]
     public class ApplicationsController : Controller
     {
         private readonly ApplicationDbContext _context;
