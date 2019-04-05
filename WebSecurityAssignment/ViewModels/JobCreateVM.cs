03cf6dd9c3012d00856889094e64111107114fdc
Vilux <mattischneider@hotmail.com>
2019-04-05 10:20:41 -0700

    Job creation form fields are required

--- WebSecurityAssignment/ViewModels/JobCreateVM.cs
+++ WebSecurityAssignment/ViewModels/JobCreateVM.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,14 +8,24 @@ namespace WebSecurityAssignment.ViewModels
 {
     public class JobCreateVM
     {
+        [Required]
         public string Title { get; set; }
+        [Required]
         public string Description { get; set; }
         public string EmployerID { get; set; }
+        [Required]
         public float Amount { get; set; }
+        [Required]
+        [Display(Name = "Date Needed")]
         public DateTime dateNeeded { get; set; }
+        [Required]
+        [Display(Name = "Expiry Date")]
         public DateTime dateExpired { get; set; }
+        [Required]
         public string Address { get; set; }
+        [Required]
         public string City { get; set; }
+        [Required]
         public string Province { get; set; }
     }
 }
