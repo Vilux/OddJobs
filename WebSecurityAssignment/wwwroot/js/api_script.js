477d9a6980bdf012c371d1162eb33b4a3337f248
Vilux <mschneider20@my.bcit.ca>
2019-09-08 13:02:28 -0700

    Fixed api script

--- WebSecurityAssignment/wwwroot/js/api_script.js
+++ WebSecurityAssignment/wwwroot/js/api_script.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    $.getJSON('http://oddjob.azurewebsites.net/api/jobfeedapi',
+    $.getJSON('https://oddjobapp.azurewebsites.net/api/jobfeedapi',
         function (data) {
             var items = data;
             console.log(items);
